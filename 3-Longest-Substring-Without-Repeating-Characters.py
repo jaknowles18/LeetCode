2e518f8071f80ab4b4885f6439c7c5bc362ad9e7
James K <jaknowles18@gmail.com>
2026-09-16 15:33:24 -0400

    [3] [Time Beats: 48.05%] [Memory Beats: 59.69%] - LeetPush

---
+++ 3-Longest-Substring-Without-Repeating-Characters.py
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> lastSeen;
+        int left = 0;
+        int longest = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            if (lastSeen.count(s[right]) && lastSeen[s[right]] >= left) {
+                left = lastSeen[s[right]] + 1;
+            }
+
+            lastSeen[s[right]] = right;
+            longest = max(longest, right - left + 1);
+        }
+
+        return longest;
+    }
+};
