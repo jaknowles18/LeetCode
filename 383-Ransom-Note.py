f65200209cc8ed3c10c82aa0cd6410fa2358f3c0
James K <jaknowles18@gmail.com>
2026-09-16 15:38:31 -0400

    [383] [Time Beats: 54.85%] [Memory Beats: 34.90%] - LeetPush

---
+++ 383-Ransom-Note.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count = [0] * 26
+
+        for ch in magazine:
+            count[ord(ch) - ord('a')] += 1
+
+        for ch in ransomNote:
+            idx = ord(ch) - ord('a')
+            count[idx] -= 1
+            if count[idx] < 0:
+                return False
+
+        return True
